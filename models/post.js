b704e6e71a413a779ddc7cf35f887cdd77fc8205
Kristin Hoffer <kristinlhoffer@gmail.com>
2016-01-22 10:39:06 -0800

    add post model without image or user reference

---
+++ models/post.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+var postSchema = new mongoose.Schema( {
+	title: String,
+	description: String,
+	location: String
+})
+
+var Post = mongoose.model('Post', postSchema);
+
+module.exports = Post

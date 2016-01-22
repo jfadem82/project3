0d310f52f681d40e82dbad33f0fd7e02813b8e23
Kristin Hoffer <kristinlhoffer@gmail.com>
2016-01-22 11:27:17 -0800

    add index posts view

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,7 +3,7 @@ var Post = require('../models/post.js')
 function index (req, res) {
 	Post.find({}, function (err, posts) {
 		if (err) console.log(err)
-		res.render('layout', {posts: posts})
+		res.render('posts.ejs');
 	})
 }
 

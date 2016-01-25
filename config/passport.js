b1a02f1deca8bff004b27335db4b03155fdf07e2
Josh <jfadem82@gmail.com>
2016-01-25 11:33:44 -0800

    Let's try this

--- config/passport.js
+++ config/passport.js
@@ -65,7 +65,7 @@ module.exports = function(passport) {
 
        clientID        : FACEBOOK_APP_ID,
        clientSecret    : FACEBOOK_APP_SECRET,
-       callbackURL     : 'https://show-me-your-junk.herokuapp.com/auth/,
+       callbackURL     : 'https://show-me-your-junk.herokuapp.com/,
        profileFields   : ["emails", "displayName", "name"]
 
    },
